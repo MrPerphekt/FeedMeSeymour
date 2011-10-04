0d47cda1ccf548eaa88830cccf58c4dc5ca5ad4c
Bret Faller <bretfaller@hotmail.com>
2011-10-03 18:14:49 -0700

    Project creation

---
+++ FeedMeSeymour/FeedMeSeymour/FeedMeSeymourAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FeedMeSeymourAppDelegate.h
+//  FeedMeSeymour
+//
+//  Created by Bret Faller on 10/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedMeSeymourAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ FeedMeSeymour/FeedMeSeymour/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  FeedMeSeymour
+//
+//  Created by Bret Faller on 10/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
